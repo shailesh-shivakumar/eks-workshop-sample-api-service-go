ed98276f9e4dd1820e287158d0bd3ea92d3a4ae4
shailesh-shivakumar <45159694+shailesh-shivakumar@users.noreply.github.com>
2022-04-21 14:40:38 +0530

    Updated for the demo

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World 21April Code for the demo"}
+		res := &response{Message: "Hello World 21April Code for the Fincare team"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
